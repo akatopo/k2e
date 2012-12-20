104d6853d678fb030f2418613c08cc6409d61f7e
Alex Katopodis <M8R-nvy63e@mailinator.com>
2012-12-20 17:39:19 +0200

    Added a simple hashing function (java's string hashCode) for caching periodical articles

---
+++ k2e/bootplate/source/String_hashCode.js
@@ -0,0 +1,15 @@
+// Source: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+
+String.prototype.hashCode = function () {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    var len = this.length
+    for (var i = 0; i < len; ++i) {
+        var c = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + c;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
