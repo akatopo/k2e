d516d711cf57d6301930d391212a8d2bde03f125
Alex Katopodis <M8R-nvy63e@mailinator.com>
2013-03-28 18:28:32 +0200

    Fixing deployment

--- k2e/bootplate/Default.aspx.cs
+++ k2e/bootplate/Default.aspx.cs
@@ -18,40 +18,5 @@ using EvernoteWebQuickstart;
 
 namespace k2e
 {
-    public partial class _Default : EvernoteWebQuickstart.EvernotePage
-    {
-        //public string Notebooks { get; set; }
 
-        //public string Clippings{ get; set; }
-
-        //private Notebook ClippingNotebook { get; set; }
-
-        protected const string CLIPPING_NOTEBOOK_NAME = "Kindle Clippings";
-
-        protected readonly static string[] DEFAULT_TAGS = 
-                new string[] { "k2e", "kindle_clippings" };
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            bool authSuccess = base.LoadPage();
-        }
-
-        [WebMethod]
-        public static string Export(DocumentArrayExport q)
-        {
-            OAuthKey accessTokenContainer = 
-                EvernoteAuthHelper.LoadCredentials().GetAccessToken();
-            
-            var exporter = new EvernoteExporter(
-                accessTokenContainer: accessTokenContainer,
-                clippingNotebookName: CLIPPING_NOTEBOOK_NAME,
-                defaultTags: DEFAULT_TAGS);
-
-            exporter.ExportClippings(q);
-
-            return ":-D";
-         
-        }
-        
-    }
 }
