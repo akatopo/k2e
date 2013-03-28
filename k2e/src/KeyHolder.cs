3ee6e716d931089b86ead29a2c849e8ff6bcfde4
Alex Katopodis <M8R-nvy63e@mailinator.com>
2013-03-28 17:09:37 +0200

    Trying to fix appharbor deployment

--- k2e/src/KeyHolder.cs
+++ k2e/src/KeyHolder.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace k2e
+﻿namespace k2e
 {
     public struct KeyHolder
     {
         // TODO: put your own credentials here: 
-        public static string PublicKey = "PublicKey";
-        public static string SecretKey = "SecretKey";
+        public static string PublicKey =
+                System.Configuration.ConfigurationManager.AppSettings["PublicKey"];
+        public static string SecretKey =
+                System.Configuration.ConfigurationManager.AppSettings["SecretKey"];
+        public static string DeveloperToken =
+                System.Configuration.ConfigurationManager.AppSettings["DeveloperToken"];
     }
 }
