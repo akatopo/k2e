e26ef1885b42c1ce05502178f01105754cd98f02
Alex Katopodis <M8R-nvy63e@mailinator.com>
2013-03-28 18:16:30 +0200

    Fixing deployment

---
+++ k2e/deploy/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+using Thrift;
+using Thrift.Protocol;
+using Thrift.Transport;
+using Evernote.EDAM.Type;
+using Evernote.EDAM.UserStore;
+using Evernote.EDAM.NoteStore;
+using Evernote.EDAM.Error;
+using EvernoteWebQuickstart;
+
+namespace k2e
+{
+    public partial class _Default : EvernoteWebQuickstart.EvernotePage
+    {
+        //public string Notebooks { get; set; }
+
+        //public string Clippings{ get; set; }
+
+        //private Notebook ClippingNotebook { get; set; }
+
+        protected const string CLIPPING_NOTEBOOK_NAME = "Kindle Clippings";
+
+        protected readonly static string[] DEFAULT_TAGS = 
+                new string[] { "k2e", "kindle_clippings" };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool authSuccess = base.LoadPage();
+        }
+
+        [WebMethod]
+        public static string Export(DocumentArrayExport q)
+        {
+            OAuthKey accessTokenContainer = 
+                EvernoteAuthHelper.LoadCredentials().GetAccessToken();
+            
+            var exporter = new EvernoteExporter(
+                accessTokenContainer: accessTokenContainer,
+                clippingNotebookName: CLIPPING_NOTEBOOK_NAME,
+                defaultTags: DEFAULT_TAGS);
+
+            exporter.ExportClippings(q);
+
+            return ":-D";
+         
+        }
+        
+    }
+}
