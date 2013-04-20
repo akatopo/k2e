0a4c090799127234edb06c4dcd92203077e6f046
Alex Katopodis <M8R-nvy63e@mailinator.com>
2013-04-21 00:42:28 +0300

    Fixed file picker for Android (tested on 4.2.2)

--- k2e/bootplate/source/ClippingPickerPopup.js
+++ k2e/bootplate/source/ClippingPickerPopup.js
@@ -18,7 +18,10 @@ enyo.kind({
     components: [
         {content: "Drag your kindle clippings here or "},
         {kind: "onyx.Button", content: "Load from File", ontap: "loadFile"},
-        {name: "file_picker", kind: "enyo.Input", type: "file", style: "display:none"}
+        {tag: "span", style: "width: 0px; height: 0px; overflow: hidden;", components: [
+            {name: "file_picker", kind: "enyo.Input", type: "file", onchange: "handleFiles"}
+        ]}
+        
     ],
 
     events: {
@@ -41,6 +44,20 @@ enyo.kind({
         this.doClippingsTextChanged();
     },
 
+    handleFiles: function (inSender, inEvent) {
+        var reader = new FileReader(),
+            files = inEvent.target.files,
+            self = this;
+
+        if (files.length > 0) {
+            reader.onload = function (e) {
+                self.setClippingsText(e.target.result);
+            };
+
+            reader.readAsText(files[0]);
+        }
+    },
+
     handleOnShow: function () {
         var self = this,
             popupNode = this.hasNode(),
@@ -53,41 +70,28 @@ enyo.kind({
 
         if (popupNode && pickerNode) {
             handleDragleave = function (ev) {
-                self.addRemoveClass("onyx-blue", false);
+                self.removeClass("onyx-blue");
             };
 
             handleDragover = function (ev) {
-                self.addRemoveClass("onyx-blue", true);
+                self.addClass("onyx-blue");
                 ev.stopPropagation();
                 ev.preventDefault();
                 ev.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
             };
 
             handleFiles = function (files) {
-                var reader = new FileReader();
-
-                if (files.length > 0) {
-                    reader.onload = function (e) {
-                        self.setClippingsText(e.target.result);
-                    };
-
-                    reader.readAsText(files[0]);
-                }
+                self.handleFiles(null, {target: {files: files}});
             };
 
             handleDrop = function (ev) {
                 ev.stopPropagation();
                 ev.preventDefault();
 
-                self.addRemoveClass("onyx-blue", false);
+                self.removeClass("onyx-blue");
                 handleFiles(ev.dataTransfer.files); // FileList object.
             };
 
-            handleFilePick = function () {
-                handleFiles(this.files); // FileList object.
-            };
-
-            pickerNode.addEventListener('change', handleFilePick, false);
             popupNode.addEventListener('dragleave', handleDragleave, false);
             popupNode.addEventListener('dragover', handleDragover, false);
             popupNode.addEventListener('drop', handleDrop, false);
