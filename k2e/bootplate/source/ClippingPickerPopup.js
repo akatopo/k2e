121e3eafb370ff97c819102588bf4969a1d33e27
Alex Katopodis <akatopo@users.noreply.github.com>
2016-09-01 19:56:07 +0300

    :bug: ClippingPickerPopup: clickable cancel button when hidden

--- k2e/bootplate/source/ClippingPickerPopup.js
+++ k2e/bootplate/source/ClippingPickerPopup.js
@@ -112,6 +112,7 @@ function handleShow() {
 function reset() {
   this.$.filePicker.hasNode().value = '';
   this.$.errorMessage.set('showing', false);
+  this.set('autoDismiss', false);
 }
 
 function autoDismissChanged(oldValue, newValue) {
